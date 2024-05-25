18c812b7ee20de7415066f5adf03bfc23ebbc986
mhgarry <mhgarry92@gmail.com>
2024-05-25 05:42:43 -0400

    added functinoality to show aria warnings and managed state for create form

--- app/dashboard/invoices/error.tsx
+++ app/dashboard/invoices/error.tsx
@@ -3,29 +3,27 @@
 import { useEffect } from 'react';
 
 export default function Error({
+  // props to pass to the component
   error,
   reset,
 }: {
-  error: Error & { digest?: string };
-  reset: () => void;
+  // definitions of prop types of props to pass to the component
+  error: Error & { digest?: string }; // error is an instanece of the rror class with an optional digest property that is a string that represents the error
+  reset: () // reset is a function that takes no arguments and returns void
+  => void; // in this instance, a reset function is passed to the component to reset the error state to default
 }) {
   useEffect(() => {
-    // Optionally log the error to an error reporting service
+    //log the error to the console
     console.error(error);
   }, [error]);
 
   return (
     <main className="flex h-full flex-col items-center justify-center">
-      <h2 className="text-center">Something went wrong!</h2>
+      <h2 className="text-center">Something went wrong!</h2>:
       <button
         className="mt-4 rounded-md bg-blue-500 px-4 py-2 text-sm text-white transition-colors hover:bg-blue-400"
-        onClick={
-          // Attempt to recover by trying to re-render the invoices route
-          () => reset()
-        }
-      >
-        Try again
-      </button>
+        onClick={() => reset()}
+      ></button>
     </main>
   );
 }
--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -1,46 +1,75 @@
 'use server';
 
-import { z } from 'zod';
+import { custom, z } from 'zod';
 import { sql } from '@vercel/postgres';
 import { revalidatePath } from 'next/cache';
 import { redirect } from 'next/navigation';
 
 const FormSchema = z.object({
   id: z.string(),
-  customerId: z.string(),
-  amount: z.coerce.number(),
-  status: z.enum(['pending', 'paid']),
+  cusotmerId: z.string({
+    invalid_type_error: 'Customer ID must be a string.',
+  }),
+  amount: z.coerce
+    .number()
+    .gt(0, { message: 'Please enter an amount greater than $0.' }),
+  status: z.enum(['pending', 'paid'], {
+    invalid_type_error: 'Please select an invoice status.',
+  }),
   date: z.string(),
 });
 
 const UpdateInvoice = FormSchema.omit({ id: true, date: true });
 
 const CreateInvoice = FormSchema.omit({ id: true, date: true });
-export async function createInvoice(formData: FormData) {
-  const { customerId, amount, status } = CreateInvoice.parse({
+
+// This is temporary until @types/react-dom is updated
+export type State = {
+  errors?: {
+    customerId?: String[];
+    amount?: String[];
+    status?: String[];
+  };
+  message?: string | null;
+};
+
+// create an async function to create an invoice
+export async function createInvoice(prevState: State, formData: FormData) {
+  // validate form fields with zod
+  const validatedFields = CreateInvoice.safeParse({
     customerId: formData.get('customerId'),
     amount: formData.get('amount'),
     status: formData.get('status'),
   });
 
+  if (!validatedFields.success) {
+    return {
+      errors: validatedFields.error.flatten.fieldErrors,
+      message: 'Missing fields. Failed to create invoice.',
+    };
+  }
+  // Prepare data for insertion into the database
+  const { customerId, amount, status } = validatedFields.data;
   const amountInCents = amount * 100;
   const date = new Date().toISOString().split('T')[0];
 
+  // Insert data into the database
   try {
     await sql`
       INSERT INTO invoices (customer_id, amount, status, date)
       VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
     `;
   } catch (error) {
+    // If a database error occurs, return a more specific error.
     return {
       message: 'Database Error: Failed to Create Invoice.',
     };
   }
 
+  // Revalidate the cache for the invoices page and redirect the user.
   revalidatePath('/dashboard/invoices');
   redirect('/dashboard/invoices');
 }
-
 export async function updateInvoice(id: string, formData: FormData) {
   const { customerId, amount, status } = UpdateInvoice.parse({
     customerId: formData.get('customerId'),
@@ -59,14 +88,9 @@ export async function updateInvoice(id: string, formData: FormData) {
   } catch (error) {
     return { message: 'Database Error: Failed to Update Invoice.' };
   }
-
-  revalidatePath('/dashboard/invoices');
-  redirect('/dashboard/invoices');
 }
 
 export async function deleteInvoice(id: string) {
-  throw new Error('Failed to Delete Invoice');
-
   try {
     await sql`DELETE FROM invoices WHERE id = ${id}`;
     revalidatePath('/dashboard/invoices');
@@ -75,3 +99,39 @@ export async function deleteInvoice(id: string) {
     return { message: 'Database Error: Failed to Delete Invoice.' };
   }
 }
+
+// export async function updateInvoice(id: string, formData: FormData) {
+//   const { customerId, amount, status } = UpdateInvoice.parse({
+//     customerId: formData.get('customerId'),
+//     amount: formData.get('amount'),
+//     status: formData.get('status'),
+//   });
+
+//   const amountInCents = amount * 100;
+
+//   try {
+//     await sql`
+//         UPDATE invoices
+//         SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
+//         WHERE id = ${id}
+//       `;
+//   } catch (error) {
+//     return { message: 'Database Error: Failed to Update Invoice.' };
+//   }
+
+//   revalidatePath('/dashboard/invoices');
+//   redirect('/dashboard/invoices');
+// }
+
+// export async function deleteInvoice(id: string) {
+//   throw new Error('Failed to delete invoice.');
+
+//   // ureachable code block
+//   try {
+//     await sql`DELETE FROM invoices WHRE id = ${id}`;
+//     revalidatePath('/dashboard/invoices');
+//     return { message: 'Deleted invoice.' };
+//   } catch (error) {
+//     return { message: 'Database error: Failed to Delete Invoice' };
+//   }
+// }
